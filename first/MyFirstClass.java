23ecec4432c0d940db06e58c9fc778ff98ba577c
Kamil Budzik <mixboksu@gmail.com>
2025-01-13 15:31:44 +0100

    Scanner

--- first/MyFirstClass.java
+++ first/MyFirstClass.java
@@ -1,5 +1,19 @@
-public class MyFirstClass {
-	 public static void main(String[] args) {
-		System.out.println("Hello " + args[0]);
-	}
+public class MultipleClassesInSameFile {
+    public static void main(String[] args) {
+ 
+        System.out.println(GenerateMessage.generateMessage());
+        System.out.println(AnotherMessage.generateAnotherMessage());
+    }
+}
+ 
+class GenerateMessage {
+    static String generateMessage() {
+        return "Here is one message";
+    }
+}
+ 
+class AnotherMessage {
+    static String generateAnotherMessage() {
+        return "Here is another message";
+    }
 }
