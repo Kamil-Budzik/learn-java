19bf358581b6b72e2995f2ad6bfa985696f62acf
Kamil Budzik <mixboksu@gmail.com>
2025-01-13 16:03:28 +0100

    Testing in Java

---
+++ Intelij-test/src/main/java/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public static double average(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return (double) sum / numbers.length;
+    }
+}
