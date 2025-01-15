bb3a92f2b0cc326946804c510aca365cac876b50
Kamil Budzik <mixboksu@gmail.com>
2025-01-15 14:23:52 +0100

    [2D] Implement GameLoop with Delta method

--- 2d-game/src/main/GamePanel.java
+++ 2d-game/src/main/GamePanel.java
@@ -39,31 +39,34 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
-        double drawInterval = (double) 1_000_000_000 / FPS; // 0.01666 seconds
-        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        double drawInterval = 1_000_000_000.0 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
 
         while(gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
 
-            update();
-
-            repaint();
-
-
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime / 1_000_000; // convert to milliseconds
-
-                if (remainingTime < 0) {
-                    remainingTime = 0;
-                }
-
-                Thread.sleep((long) remainingTime);
-
-                nextDrawTime += drawInterval;
-
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
             }
+
+            if(timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
+
         }
     }
 
