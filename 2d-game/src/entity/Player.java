4b37c2281714437dec7fbb4b74e3921d322017b9
Kamil Budzik <mixboksu@gmail.com>
2025-01-17 18:21:01 +0100

    [2D] Add player movement animation

--- 2d-game/src/entity/Player.java
+++ 2d-game/src/entity/Player.java
@@ -47,31 +47,66 @@ public class Player extends Entity {
     }
 
     public void update() {
-        if(keyH.upPressed) {
-            direction = "up";
-            y -= speed;
-        } else if (keyH.downPressed) {
-            direction = "down";
-            y += speed;
-        } else if (keyH.leftPressed) {
-            direction = "left";
-            x -= speed;
-        } else if (keyH.rightPressed) {
-            direction = "right";
-            x += speed;
+
+        if (keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
+            if(keyH.upPressed) {
+                direction = "up";
+                y -= speed;
+            } else if (keyH.downPressed) {
+                direction = "down";
+                y += speed;
+            } else if (keyH.leftPressed) {
+                direction = "left";
+                x -= speed;
+            } else {
+                direction = "right";
+                x += speed;
+            }
+
+            spriteCounter++;
+            if(spriteCounter > 12) {
+                spriteCounter = 0;
+                spriteNum = (spriteNum == 1) ? 2 : 1;
+            }
         }
+
+
     }
 
     public void draw(Graphics2D g2) {
 //        g2.setColor(Color.white);
 //        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
-        BufferedImage image = switch (direction) {
-            case "up" -> up1;
-            case "down" -> down1;
-            case "left" -> left1;
-            case "right" -> right1;
-            default -> null;
-        };
+        BufferedImage image = null;
+        switch(direction) {
+            case "up":
+                if (spriteNum == 2) {
+                    image = up1;
+                } else {
+                    image = up2;
+                }
+                break;
+            case "down":
+                if (spriteNum == 2) {
+                    image = down1;
+                } else {
+                    image = down2;
+                }
+                break;
+            case "left":
+                if (spriteNum == 2) {
+                    image = left1;
+                } else {
+                    image = left2;
+                }
+                break;
+            case "right":
+                if (spriteNum == 2) {
+                    image = right1;
+                } else {
+                    image = right2;
+                }
+                break;
+        }
 
         g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
