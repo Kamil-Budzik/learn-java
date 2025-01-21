5e25bc1d5129f5fd28dc3e16d90270e80585a238
Kamil Budzik <mixboksu@gmail.com>
2025-01-21 19:18:03 +0100

    [2D] Load and import a map

--- 2d-game/src/tile/TileManager.java
+++ 2d-game/src/tile/TileManager.java
@@ -3,18 +3,26 @@ package tile;
 import main.GamePanel;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TileManager {
 
     GamePanel gp;
     Tile[] tile;
+    int[][] mapTileNum;
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
 
         tile = new Tile[10];
+        mapTileNum = new int[gp.maxScreenRow][gp.maxScreenCol];
 
         getTileImage();
+        loadMap("/maps/map01.txt");
     }
 
     public void getTileImage() {
@@ -28,6 +36,42 @@ public class TileManager {
         tile[2].image = gp.getImage("tiles/water.png");
     }
 
+    public void loadMap(String path) {
+        try {
+            InputStream is = getClass().getResourceAsStream(path);
+            if (is == null) {
+                System.out.println("Map file not found");
+                return;
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int col = 0;
+            int row = 0;
+
+            while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+                String line = br.readLine();
+
+                while (col < gp.maxScreenCol) {
+                    String[] numbers = line.split(" ");
+
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[row][col] = num;
+                    col++;
+                }
+
+                if (col == gp.maxScreenCol) {
+                    col = 0;
+                    row++;
+                }
+            }
+            br.close();
+
+        } catch (Exception e) {
+            Logger.getLogger(TileManager.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
     public void draw(Graphics2D g2) {
         int col = 0;
         int row = 0;
@@ -35,11 +79,14 @@ public class TileManager {
         int y = 0;
 
         while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
-            g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
+
+            int tileNum = mapTileNum[row][col];
+
+            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
             col++;
             x += gp.tileSize;
 
-            if(col == gp.maxScreenCol) {
+            if (col == gp.maxScreenCol) {
                 col = 0;
                 x = 0;
                 row++;
